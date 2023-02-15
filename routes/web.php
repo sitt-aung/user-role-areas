ab7c0e4e02600e668be88aa45ac49c8f482cf8c6
Sitt Aung <sittaung07@gmail.com>
2023-02-15 14:00:01 +0630

    Create student timetable page

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/student/timetable', [\App\Http\Controllers\Student\TimetableController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('student.timetable');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
