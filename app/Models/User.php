abbca058572a9862cca884ebba64f0d8a4f7a056
Sitt Aung <sittaung07@gmail.com>
2023-02-15 14:55:59 +0630

    Auto-redirect to correct area after login/register

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getRedirectRouteName()
+    {
+        return match ((int)$this->role_id) {
+            1 => 'student.timetable',
+            2 => 'teacher.timetable',
+        };
+    }
 }
