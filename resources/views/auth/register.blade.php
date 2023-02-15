1c5b3c021d8c37bc184e820460d5b8e50bd117e3
Sitt Aung <sittaung07@gmail.com>
2023-02-15 14:31:04 +0630

    Role model, migrations, seeder and teacher registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,6 +39,21 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <!-- Role -->
+        <div class="mt-4">
+            <x-input-label for="role_id" :value="__('Register as:')" />
+
+            <label>
+                <input type="radio" name="role_id" value="1" checked /> Student
+            </label>
+
+            <label class="ml-2">
+                <input type="radio" name="role_id" value="2" /> Teacher
+            </label>
+
+            <x-input-error :messages="$errors->get('role_id')" class="mt-2" />
+        </div>
+
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
